5c1e79f98b047f9e15534d55544694362f7b28b4
MJOW1999 <michael.jow.williams@gmail.com>
2023-04-20 04:26:32 -0700

    Remove unused files

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import basketReducer from "./slices/basketSlice";
 
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
-    counter: counterReducer,
   },
 });
